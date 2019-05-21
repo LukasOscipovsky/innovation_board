ee35180ac13b2409c5da327c3fca141e1f527d5a
Lukas Oscipovsky <lukas.oscipovsky@performgroup.com>
2019-05-21 15:13:45 +0200

    added team bar handling and add plus button

--- innovation-board/src/components/Innovation.tsx
+++ innovation-board/src/components/Innovation.tsx
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Modal from '@material-ui/core/Modal';
 
 interface InnovationState {
   open: boolean;
@@ -20,60 +17,9 @@ class Innovation extends Component<IProps, InnovationState> {
         title: ''
     }
 }
-
-    handleOpen = () => {
-      this.setState({ open: true });
-    };
-
-    handleClose = () => {
-      this.setState({ open: false });
-    };
-
     render() {
         return (
           <div className='InnovationDiv'>
-            <Modal
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-            open={this.state.open}
-            onClose={this.handleClose}
-            >
-                <div className="InnovationModal">
-                    <label className="ModalTitle">INNOVATION</label>
-                        <TextField
-                        required
-                        label="InnovationTitle" 
-                        onChange={event => this.setState({title: event.currentTarget.value})} 
-                        placeholder="Innovation Title"
-                        variant="outlined"
-                        style={{fontFamily: 'Trim,DAZN-Bold,Oscine', outlineColor: 'black'}}
-                        margin='normal'
-                        />
-                        <TextField
-                        required
-                        label="InnovationDescription" 
-                        style={{fontFamily: 'Trim,DAZN-Bold,Oscine', outlineColor: 'black'}}
-                        placeholder="Innovation Description"
-                        variant="outlined"
-                        margin='normal'
-                        multiline={true}
-                        rows={5}
-                        rowsMax={10}>
-                        </TextField>
-                    <div className="ModalActions">
-                        <Button
-                        style={{backgroundColor: '#242d34', marginRight: 20, color: '#f8fc00', fontFamily: 'Trim,Now-Bold,Oscine'}}
-                        onClick={this.handleClose}>
-                        Save
-                        </Button>
-                        <Button
-                        style={{backgroundColor: '#242d34', color: '#DC143C', fontFamily: 'Trim,Now-Bold,Oscine'}} 
-                        onClick={this.handleClose}>
-                        Close
-                        </Button>
-                    </div>
-                </div>
-            </Modal>
           </div>
         );
       }
--- innovation-board/src/components/TeamBar.tsx
+++ innovation-board/src/components/TeamBar.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import Innovation from './Innovation';
+import Add from '../assets/add-plus-button.png';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Modal from '@material-ui/core/Modal';
+import InnovationDTO from '../data/InnovationDTO';
 
 interface TeamState {
   open: boolean;
   name: string;
+  ins: Array<InnovationDTO>
 }
 
 interface TeamProps {
@@ -15,7 +20,8 @@ class TeamBar extends Component<TeamProps, TeamState> {
     super(props);
       this.state = {
         open: false,
-        name: this.props.name
+        name: this.props.name,
+        ins: []
       }
     }
   
@@ -32,10 +38,60 @@ class TeamBar extends Component<TeamProps, TeamState> {
           <div className='TeamBar'>
             <div className='FormDiv'>
               <label className='FormLabel'>{this.state.name}</label>
-              <div className="PlusDiv">
+              <div className="PlusDiv" onClick={this.handleOpen}>
+                <img src={Add} className="Add-icon" alt="add"/>
               </div>
             </div>
-            <Innovation open={this.state.open}/>
+            <div className='InnovationContainer'>
+            </div>
+            <Modal
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            open={this.state.open}
+            onClose={this.handleClose}
+            >
+                <div className="InnovationModal">
+                    <label className="ModalTitle">INNOVATION</label>
+                      <div className="TextFieldDiv">
+                        <TextField
+                        required
+                        label="InnovationTitle" 
+                        /* onChange={event => this.setState({title: event.currentTarget.value})} */
+                        placeholder="Innovation Title"
+                        variant="outlined"
+                        style={{fontFamily: 'Trim,DAZN-Bold,Oscine', outli: 10}}
+                        margin="normal"
+                        fullWidth={true}
+                        />
+                        </div>
+                        <div className="TextFieldDiv">
+                        <TextField
+                        required
+                        label="InnovationDescription" 
+                        style={{fontFamily: 'Trim,DAZN-Bold,Oscine', outlineColor: 'black'}}
+                        placeholder="Innovation Description"
+                        variant="outlined"
+                        multiline={true}
+                        fullWidth
+                        margin="normal"
+                        rows={8}
+                        rowsMax={10}>
+                        </TextField>
+                        </div>
+                    <div className="ModalActions">
+                        <Button
+                        style={{backgroundColor: '#242d34', marginRight: 20, color: '#f8fc00', fontFamily: 'Trim,DAZN-Bold,Oscine'}}
+                        onClick={this.handleClose}>
+                        Save
+                        </Button>
+                        <Button
+                        style={{backgroundColor: '#242d34', color: '#DC143C', fontFamily: 'Trim,DAZN-Bold,Oscine'}} 
+                        onClick={this.handleClose}>
+                        Close
+                        </Button>
+                    </div>
+                </div>
+            </Modal>
           </div>
         );
       }
