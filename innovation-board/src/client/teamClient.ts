0f8cca4ba72552cde6c2d2f8e43e42883e78783b
LukasOscipovsky <lukas.oscipovsky@gmail.com>
2019-09-26 17:34:38 +0200

    cahnged api to 5555

--- innovation-board/src/client/teamClient.ts
+++ innovation-board/src/client/teamClient.ts
@@ -4,17 +4,17 @@ import jsonConverter from '../mapper/jsonConverter';
 
 
 export const getTeams = async () => {
-  return await axios.get('http://localhost:8080/team')
+  return await axios.get('http://localhost:5555/team')
     .then(r => r.data)
     .then(data => jsonConverter().deserializeArray(data, TeamDTO));
 }
 
 export const saveTeam = async (team: TeamDTO) => {
-  return await axios.put('http://localhost:8080/team', team)
+  return await axios.put('http://localhost:5555/team', team)
     .then(r => r, err => { console.log(err) })
 }
 
 export const deleteTeam = async (teamName: string) => {
-  return await axios.delete('http://localhost:8080/team/' + teamName)
+  return await axios.delete('http://localhost:5555/team/' + teamName)
     .then(r => r, err => { console.log(err) })
 }
