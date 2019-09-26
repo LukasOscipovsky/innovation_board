8de6557a4ca310948709f83c340e939c05e9c3ad
LukasOscipovsky <lukas.oscipovsky@gmail.com>
2019-09-26 18:00:44 +0200

    teamDto

--- innovation-board/src/App.tsx
+++ innovation-board/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './assets/DAZN-hero-updated.png';
 import './App.css';
-import TeamDTO from './data/TeamDTO';
+import TeamDTO from './data/teamDTO';
 import TeamBar from './components/TeamBar';
 import AddBox from '@material-ui/icons/AddBoxTwoTone';
 import TeamModal from './modals/TeamModal';
--- innovation-board/src/modals/TeamModal.tsx
+++ innovation-board/src/modals/TeamModal.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Modal from '@material-ui/core/Modal';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import TeamDTO from '../data/TeamDTO';
+import TeamDTO from '../data/teamDTO';
 
 interface TeamModalState {
     open: boolean;
