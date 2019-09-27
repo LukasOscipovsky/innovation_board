60cd1274d459f22c1d0b3df05ad7d93d250b45b6
Lukas Oscipovsky <lukas.oscipovsky@performgroup.com>
2019-09-27 07:46:28 +0200

    change to dev ip

--- innovation-board/src/client/teamClient.ts
+++ innovation-board/src/client/teamClient.ts
@@ -4,17 +4,17 @@ import jsonConverter from '../mapper/jsonConverter';
 
 
 export const getTeams = async () => {
-  return await axios.get('http://172.18.0.1:5555/team')
+  return await axios.get('http://10.219.227.17:5555/team')
     .then(r => r.data)
     .then(data => jsonConverter().deserializeArray(data, TeamDTO));
 }
 
 export const saveTeam = async (team: TeamDTO) => {
-  return await axios.put('http://172.18.0.1:5555/team', team)
+  return await axios.put('http://10.219.227.17:5555/team', team)
     .then(r => r, err => { console.log(err) })
 }
 
 export const deleteTeam = async (teamName: string) => {
-  return await axios.delete('http://172.18.0.1:5555/team/' + teamName)
+  return await axios.delete('http://10.219.227.17:5555/team/' + teamName)
     .then(r => r, err => { console.log(err) })
 }
