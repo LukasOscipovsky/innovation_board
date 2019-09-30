b510a3c5de3117826a43e282291f5deef4fd3f62
Lukas Oscipovsky <lukas.oscipovsky@performgroup.com>
2019-09-30 07:15:50 +0200

    changed for linux

--- innovation-api/src/client/mongoDbClient.ts
+++ innovation-api/src/client/mongoDbClient.ts
@@ -1,6 +1,6 @@
 import mongo = require('mongodb')
 
-const url: string = 'mongodb://127.0.0.1:2222';
+const url: string = 'mongodb://172.18.0.1:2222';
 const mongoClient = mongo.MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 export async function getMongoConnection() { return await mongoClient; }
